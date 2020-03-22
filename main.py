b9914b2e59a97b174dd5755affb0b5fd2fae711d
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-03-22 19:56:26 -0300

    Add command line parameters parsing

--- main.py
+++ main.py
@@ -4,4 +4,17 @@ async def main():
     pass
 
 if __name__ == "__main__":
+    import argparse
+    import exitstatus
+
+    parser = argparse.ArgumentParser(description="Start a file server.")
+
+    parser.add_argument("-f", "--file", type=str,
+        help="file to be served")
+
+    parser.add_argument("-c", "--ncon", type=int, default=10,
+        help="number of consumers")
+
+    args = parser.parse_args()
+
     asyncio.run(main())
