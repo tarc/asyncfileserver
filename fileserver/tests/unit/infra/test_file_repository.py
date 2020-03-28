f8298dbf3eeb02ddde3f6fa902fcd292cd38003f
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-03-28 01:33:35 -0300

    Testing for file size less than the others

--- fileserver/tests/unit/infra/test_file_repository.py
+++ fileserver/tests/unit/infra/test_file_repository.py
@@ -33,8 +33,6 @@ class TestFileRepository(aiounittest.AsyncTestCase):
     async def test_read_buffer_size_lt_file_size_lt_queue_item_size(self):
         repository = Repository(ByteArrayFile(bytearray(b"\x01\x02\x03\x04\x05")),
                                 read_buffer_size=1, queue_item_size=6)
-
-        #result = [bytearray([i+1]) for i in range(5)]
         items = []
         async for item in repository.data():
             items.append(item)
@@ -44,7 +42,6 @@ class TestFileRepository(aiounittest.AsyncTestCase):
     async def test_read_buffer_size_lt_file_size_eq_queue_item_size(self):
         repository = Repository(ByteArrayFile(bytearray(b"\x01\x02\x03\x04\x05")),
                                 read_buffer_size=1, queue_item_size=5)
-
         items = []
         async for item in repository.data():
             items.append(item)
@@ -54,7 +51,6 @@ class TestFileRepository(aiounittest.AsyncTestCase):
     async def test_read_buffer_size_lt_file_size_gt_queue_item_size(self):
         repository = Repository(ByteArrayFile(bytearray(b"\x01\x02\x03\x04\x05")),
                                 read_buffer_size=1, queue_item_size=4)
-
         result = [
             bytearray(b"\x01\x02\x03\x04"),
             bytearray(b"\x05")
@@ -70,7 +66,6 @@ class TestFileRepository(aiounittest.AsyncTestCase):
     async def test_read_buffer_size_eq_queue_item_size_lt_file_size(self):
         repository = Repository(ByteArrayFile(bytearray(b"\x01\x02\x03\x04\x05")),
                                 read_buffer_size=1, queue_item_size=1)
-
         result = [bytearray([i+1]) for i in range(5)]
 
         items = []
@@ -79,3 +74,39 @@ class TestFileRepository(aiounittest.AsyncTestCase):
             items.append(item)
 
         self.assertTrue(items == result)
+
+    async def test_file_size_lt_read_buffer_size_lt_queue_item_size(self):
+        repository = Repository(ByteArrayFile(bytearray(b"\x01\x02\x03\x04\x05")),
+                                read_buffer_size=6, queue_item_size=7)
+        items = []
+        async for item in repository.data():
+            items.append(item)
+
+        self.assertTrue(items == [bytearray(b"\x01\x02\x03\x04\x05")])
+
+    async def test_file_size_lt_read_buffer_size_eq_queue_item_size(self):
+        repository = Repository(ByteArrayFile(bytearray(b"\x01\x02\x03\x04\x05")),
+                                read_buffer_size=6, queue_item_size=6)
+        items = []
+        async for item in repository.data():
+            items.append(item)
+
+        self.assertTrue(items == [bytearray(b"\x01\x02\x03\x04\x05")])
+
+    async def test_file_size_lt_queue_item_size_lt_read_buffer_size(self):
+        repository = Repository(ByteArrayFile(bytearray(b"\x01\x02\x03\x04\x05")),
+                                read_buffer_size=7, queue_item_size=6)
+        items = []
+        async for item in repository.data():
+            items.append(item)
+
+        self.assertTrue(items == [bytearray(b"\x01\x02\x03\x04\x05")])
+
+    async def test_file_size_eq_queue_item_size_lt_read_buffer_size(self):
+        repository = Repository(ByteArrayFile(bytearray(b"\x01\x02\x03\x04\x05")),
+                                read_buffer_size=6, queue_item_size=5)
+        items = []
+        async for item in repository.data():
+            items.append(item)
+
+        self.assertTrue(items == [bytearray(b"\x01\x02\x03\x04\x05")])
