c2cc31a2f0708b5bd5f0d3326a9975130591192c
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-03-29 20:57:06 -0300

    Add ConfirmPutQueue class

---
+++ asyncfileserver/model/confirm_put_queue.py
@@ -0,0 +1,13 @@
+class ConfirmPutQueue(object):
+    def __init__(self, arbiter):
+        self._arbiter = arbiter
+
+    async def get(self) -> bytes:
+        item: bytearray = await self._queue.get()
+        return bytes(item) if item else None
+
+    def task_done(self):
+        self._queue.task_done()
+
+    async def put(self, item: bytearray):
+        await self._queue.put(item)
