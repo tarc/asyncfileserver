5c8a778942e5ec69f52e2de8f3e43e300217e790
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-03-29 02:55:55 -0300

    Remove write buffer size from Console client

--- fileserver/infra/console.py
+++ fileserver/infra/console.py
@@ -5,10 +5,8 @@ from fileserver.infra.async_console_output import AsyncConsoleOutput as Out
 
 
 class Client(object):
-    def __init__(self, queue, output=Out(),
-                 writing_buffer_size: int = 17):
+    def __init__(self, queue, output=Out()):
         self._queue = queue
-        self._buffer_size = writing_buffer_size
         self._output = output
 
     async def write(self):
