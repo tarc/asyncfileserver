a98de936d649c72cc769037fecd6c230b3d7560b
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-03-31 03:27:51 -0300

    Interleaves a put() and a get() for async calls in FakeAsyncQueue

--- asyncfileserver/tests/unit/model/fake_async_queue.py
+++ asyncfileserver/tests/unit/model/fake_async_queue.py
@@ -8,14 +8,10 @@ class FakeAsyncQueue(object):
 
     async def put(self, item):
         self._queue.append(item)
-
-    async def get(self):
-
-        # Release control back to event loop. This is needed because for
-        # File.data() to work properly it must get a chance to read the file
-        # before getting a item from the queue.
         await asyncio.sleep(0)
 
+    async def get(self):
+        await asyncio.sleep(0)
         return self._queue[self._count_task_done]
 
     def task_done(self):
