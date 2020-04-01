6ac7fb918919be66683e11805480cc57ba75ed84
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-04-01 11:07:21 -0300

    Formatting presentation

--- asyncfileserver/model/arbiter.py
+++ asyncfileserver/model/arbiter.py
@@ -1,9 +1,18 @@
+from asyncfileserver.model.view_data import ViewData
+
+
 class Arbiter(object):
     def __init__(self, input, output):
         self._input = input
         self._output = output
+        self._continue = False
 
     async def should_put(self, item):
-        await self._output.print(item)
+        if self._continue:
+            return True
+
+        view_item = ViewData(item)
+        await self._output.print(f"\n{view_item}\n> ")
         command = await self._input.input()
-        return command == b"Y\n"
+        self._continue = command == b"C\n"
+        return self._continue or command == b"Y\n"
