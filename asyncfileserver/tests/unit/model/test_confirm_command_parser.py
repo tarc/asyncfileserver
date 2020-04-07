03519f8896a63fd6dc137a54e4f5185e719d6c92
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-04-07 04:49:17 -0300

    Improving confirm command

--- asyncfileserver/tests/unit/model/test_confirm_command_parser.py
+++ asyncfileserver/tests/unit/model/test_confirm_command_parser.py
@@ -1,11 +1,37 @@
 import unittest
 
 from asyncfileserver.model.confirm_command_parser import ConfirmCommandParser
+from asyncfileserver.model.confirm_command import ConfirmCommand
 
 class TestConfirmCommandParser(unittest.TestCase):
-    def test_create(self):
+    def test_single_char_eol(self):
         parser = ConfirmCommandParser()
-        instance = parser.parse(bytearray(b'Y\n'))
+        instance, index = parser.parse(b'y\n')
 
+        self.assertEqual(index, 2)
         self.assertFalse(instance.go_on())
         self.assertTrue(instance.yes())
+
+    def test_single_char(self):
+        parser = ConfirmCommandParser()
+        instance, index = parser.parse(b'y')
+
+        self.assertEqual(index, 0)
+        self.assertIsNone(instance)
+
+    def test_incomplete_cmd_and_then_complete(self):
+        parser = ConfirmCommandParser()
+        instance, index = parser.parse(b'y')
+
+        self.assertEqual(index, 0)
+        self.assertIsNone(instance)
+
+        instance, index = parser.parse(b'y ')
+
+        self.assertEqual(index, 2)
+        self.assertIsNone(instance)
+
+        instance, index = parser.parse(b'\n')
+
+        self.assertEqual(index, 1)
+        self.assertEqual(instance, ConfirmCommand(b'Y'))
