c86241b10e6e6142a80eecbeb27e59083b9fba0e
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-04-07 05:23:25 -0300

    Test confirm command parser

--- asyncfileserver/tests/unit/model/test_confirm_command_parser.py
+++ asyncfileserver/tests/unit/model/test_confirm_command_parser.py
@@ -35,3 +35,87 @@ class TestConfirmCommandParser(unittest.TestCase):
 
         self.assertEqual(index, 1)
         self.assertEqual(instance, ConfirmCommand(b'Y'))
+
+    def test_some_iterations(self):
+        parser = ConfirmCommandParser()
+        instance, index = parser.parse(b'y')
+
+        self.assertEqual(index, 0)
+        self.assertIsNone(instance)
+
+        instance, index = parser.parse(b'yl')
+
+        self.assertEqual(index, 2)
+        self.assertIsNone(instance)
+
+        instance, index = parser.parse(b'y\n')
+
+        self.assertEqual(index, 2)
+        self.assertEqual(instance, ConfirmCommand(b'YL', True))
+
+        instance, index = parser.parse(b'c')
+
+        self.assertEqual(index, 0)
+        self.assertEqual(instance, None)
+
+        instance, index = parser.parse(b'C\n')
+
+        self.assertEqual(index, 2)
+        self.assertEqual(instance, ConfirmCommand(b'C'))
+
+        instance, index = parser.parse(b'\n')
+
+        self.assertEqual(index, 1)
+        self.assertEqual(instance, ConfirmCommand(b'\n', True))
+
+    def test_some_iterations_r(self):
+        parser = ConfirmCommandParser()
+        instance, index = parser.parse(b'y')
+
+        self.assertEqual(index, 0)
+        self.assertIsNone(instance)
+
+        instance, index = parser.parse(b'yl')
+
+        self.assertEqual(index, 2)
+        self.assertIsNone(instance)
+
+        instance, index = parser.parse(b'y\r\n')
+
+        self.assertEqual(index, 3)
+        self.assertEqual(instance, ConfirmCommand(b'YL', True))
+
+        instance, index = parser.parse(b'c')
+
+        self.assertEqual(index, 0)
+        self.assertEqual(instance, None)
+
+        instance, index = parser.parse(b'C\r\n')
+
+        self.assertEqual(index, 3)
+        self.assertEqual(instance, ConfirmCommand(b'C'))
+
+        instance, index = parser.parse(b'C\r\r\n')
+
+        self.assertEqual(index, 4)
+        self.assertEqual(instance, ConfirmCommand(b'C'))
+
+        instance, index = parser.parse(b'\r')
+
+        self.assertEqual(index, 1)
+        self.assertEqual(instance, None)
+
+        instance, index = parser.parse(b'\n')
+
+        self.assertEqual(index, 1)
+        self.assertEqual(instance, ConfirmCommand(b'\r', True))
+
+        instance, index = parser.parse(b'\r\n')
+
+        self.assertEqual(index, 2)
+        self.assertEqual(instance, ConfirmCommand(b'\r', True))
+
+        instance, index = parser.parse(b'12345\r\r\r\n')
+
+        self.assertEqual(index, 9)
+        self.assertEqual(instance, ConfirmCommand(b'1', True))
