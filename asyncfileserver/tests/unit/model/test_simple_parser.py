afa92b5be61953e4c6a299de2fd41d32f2ad413d
Tarcisio Rodrigues <tarcisio.genaro@gmail.com>
2020-04-09 03:00:04 -0300

    Test more command sequences

--- asyncfileserver/tests/unit/model/test_simple_parser.py
+++ asyncfileserver/tests/unit/model/test_simple_parser.py
@@ -5,13 +5,10 @@ from asyncfileserver.infra.pytrie import TrieFactory
 
 
 class TestSimpleParser(unittest.TestCase):
-    def test_parse_intersecting_commands(self):
+    def test_parse_one_command_prefix_of_the_other(self):
 
-        def AB(data):
-            pass
-
-        def A(data):
-            pass
+        AB = object()
+        A = object()
 
         tag_commands = [
             b'AB',
@@ -37,7 +34,37 @@ class TestSimpleParser(unittest.TestCase):
         self.assertIsNone(command)
         self.assertEqual(size, 2)
 
-        command, size = parser.parse(b'\n')
+        command, size = parser.parse(b'\nAB')
 
         self.assertEqual(command, (A, b'A '))
         self.assertEqual(size, 1)
+
+        command, size = parser.parse(b'AB\r')
+
+        self.assertIsNone(command)
+        self.assertEqual(size, 3)
+
+        command, size = parser.parse(b'\nAB')
+
+        self.assertEqual(command, (AB, b'AB\r'))
+        self.assertEqual(size, 1)
+
+        command, size = parser.parse(b'AB')
+
+        self.assertIsNone(command)
+        self.assertEqual(size, 0)
+
+        command, size = parser.parse(b'ABA')
+
+        self.assertIsNone(command)
+        self.assertEqual(size, 3)
+
+        command, size = parser.parse(b'\r')
+
+        self.assertIsNone(command)
+        self.assertEqual(size, 1)
+
+        command, size = parser.parse(b'A AB A AB\nA')
+
+        self.assertEqual(command, (error, b'ABA'))
+        self.assertEqual(size, 10)
